3978506a82b8562c6ffcd68feb5c563b56d7239a
limzhilit <g00440629@atu.ie>
2025-11-26 14:11:24 +0000

    pull all latest method

--- PlayGame.py
+++ PlayGame.py
@@ -4,40 +4,44 @@
 # representing board
 board = [" " for _ in range(9)]
 
+
 # function to display board
 def print_board(board):
-    print(board[0] + "|" + board[1] + "|" + board[2])
-    print("-+-+-")
-    print(board[3] + "|" + board[4] + "|" + board[5])
-    print("-+-+-")
-    print(board[6] + "|" + board[7] + "|" + board[8])
+  print(board[0] + "|" + board[1] + "|" + board[2])
+  print("-+-+-")
+  print(board[3] + "|" + board[4] + "|" + board[5])
+  print("-+-+-")
+  print(board[6] + "|" + board[7] + "|" + board[8])
+
 
 # function to switch player
 def switch_player(current):
-    return "O" if current == "X" else "X"
+  return "O" if current == "X" else "X"
+
 
 # core method to play a turn
 def play_turn(board, current_player):
-    # ask the current player for a position 1–9
-    choice = int(input(f"Player {current_player}, choose a box (1-9): ")) - 1
+  # ask the current player for a position 1–9
+  choice = int(input(f"Player {current_player}, choose a box (1-9): ")) - 1
 
-    # make sure it’s empty
-    if board[choice] == " ":
-        board[choice] = current_player
-    else:
-        print("That spot is taken! Try again.")
-        return current_player  # same player tries again
+  # make sure it’s empty
+  if board[choice] == " ":
+    board[choice] = current_player
+  else:
+    print("That spot is taken! Try again.")
+    return current_player  # same player tries again
 
-    return switch_player(current_player)
+  return switch_player(current_player)
+
 
 # game loop
 def play_game():
-    board = [" " for _ in range(9)]
-    current_player = "X"
+  board = [" " for _ in range(9)]
+  current_player = "X"
 
-    for _ in range(9):  # max moves
-        print_board(board)
-        current_player = play_turn(board, current_player)
-
+  for _ in range(9):  # max moves
     print_board(board)
-    print("Game over!")
+    current_player = play_turn(board, current_player)
+
+  print_board(board)
+  print("Game over!")
--- checkWin.py
+++ checkWin.py
@@ -1,44 +1,47 @@
 SIZE = 3
 
+
 def print_victory():
-    print("You won, game is over")
+  print("You won, game is over")
+
 
 def print_defeat():
-    print("You lost, game is over")
+  print("You lost, game is over")
+
 
 def check_winner(board, data):
-    # checking rows
-    for row in range(SIZE):
-        if board[row][0] == board[row][1] == board[row][2]:
-            if data % 2 == 0:
-                print_victory()
-            else:
-                print_defeat()
-            return 1
+  # checking rows
+  for row in range(SIZE):
+    if board[row][0] == board[row][1] == board[row][2]:
+      if data % 2 == 0:
+        print_victory()
+      else:
+        print_defeat()
+      return 1
 
-    # checking columns
-    for col in range(SIZE):
-        if board[0][col] == board[1][col] == board[2][col]:
-            if data % 2 == 0:
-                print_victory()
-            else:
-                print_defeat()
-            return 1
+  # checking columns
+  for col in range(SIZE):
+    if board[0][col] == board[1][col] == board[2][col]:
+      if data % 2 == 0:
+        print_victory()
+      else:
+        print_defeat()
+      return 1
 
-    # checking main diagonal
-    if board[0][0] == board[1][1] == board[2][2]:
-        if data % 2 == 0:
-            print_victory()
-        else:
-            print_defeat()
-        return 1
+  # checking main diagonal
+  if board[0][0] == board[1][1] == board[2][2]:
+    if data % 2 == 0:
+      print_victory()
+    else:
+      print_defeat()
+    return 1
 
-    # checking anti-diagonal
-    if board[2][0] == board[1][1] == board[0][2]:
-        if data % 2 == 0:
-            print_victory()
-        else:
-            print_defeat()
-        return 1
+  # checking anti-diagonal
+  if board[2][0] == board[1][1] == board[0][2]:
+    if data % 2 == 0:
+      print_victory()
+    else:
+      print_defeat()
+    return 1
 
-    return 0
+  return 0
