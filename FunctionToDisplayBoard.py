ce5f2bbcf829e60edef4573f3c8c13ae7e6bacc0
OisinATU <G00470518@atu.ie>
2025-12-04 03:08:36 +0000

    Added numbers to the board to make choosing a position easier. Changed the color of these numbers to differentiate them from the x and o's.

--- FunctionToDisplayBoard.py
+++ FunctionToDisplayBoard.py
@@ -1,8 +1,18 @@
 
+CYAN = "\033[96m"
+WHITE = "\033[97m"
+RESET = "\033[0m"
+
 def print_board(board):
-    """ Displays the current state of the board. """
-    print(board[0] + "|" + board[1] + "|" + board[2])
+    display = []
+    for i in range(9):
+        if board[i] == " ":
+            display.append(f"{CYAN}{i+1}{RESET}")
+        else:
+            display.append(f"{WHITE}{board[i]}{RESET}")
+
+    print(display[0] + "|" + display[1] + "|" + display[2])
     print("-+-+-")
-    print(board[3] + "|" + board[4] + "|" + board[5])
+    print(display[3] + "|" + display[4] + "|" + display[5])
     print("-+-+-")
-    print(board[6] + "|" + board[7] + "|" + board[8])
+    print(display[6] + "|" + display[7] + "|" + display[8])
